0911245d68a0d4ff4907ad391f127ae45b8bbc55
BanjoRiver <32068455+BanjoRiver@users.noreply.github.com>
2018-06-07 15:22:57 -0400

    River's attempt
    I wanted to try this out and I'm not sure if you are going to review this or what but as far as I know it works.

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
 // Do Stuff Here
+for ($i=1; $i < 101; $i++) {
+    $fizz = $i % 3;
+    $buzz = $i % 5;
+    $fizzbuzz = $i % 15;
+    if($fizzbuzz == 0){
+        echo "<p>FizzBuzz</p>";
+    }elseif($fizz == 0){
+        echo "<p>Fizz</p>";
+    }elseif($buzz == 0){
+        echo "<p>Buzz</p>";
+    }else{
+        echo "<p>$i</p>";
+    }
+}
+?>
